5219b5a530b095ba3f9060eaf7187aa403c76c6b
drswinghead <drswinghead@gmail.com>
2017-05-13 10:04:05 +0800

    add libstrophe swig

---
+++ libstrophe-swig/c.go
@@ -0,0 +1,7 @@
+package libstrophe
+
+/*
+#cgo pkg-config: libstrophe
+#cgo CFLAGS: -std=c99
+*/
+import "C"
---
+++ libstrophe-swig/fixsym.h
@@ -0,0 +1,9 @@
+#ifndef _FIXSYM_H_
+#define _FIXSYM_H_
+
+#include <glib.h>
+
+#include "libpurple/core.h"
+
+#endif
+
